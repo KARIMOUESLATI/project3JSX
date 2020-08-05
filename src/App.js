7078174a33e2939976375ad9c6fb424a9ad5291a
karim oueslati <karimoueslati16@gmail.com>
2020-08-05 12:05:18 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <>
+    
+    <div style={{border:"solid 1px black" ,maxWidth:"100vw" }} >
+    <h1 className="title" red>Your name here</h1>
+          
+         <br/>
+          <img src="/imageInSrc.jpg"/> <br/> 
+
+             <img src="/imageInPublic.jpg"/>  
+
+  </div>  
+   
+  <iframe src="https://www.youtube.com/watch?v=9GtB5G2xGTY" width="540" height="450"></iframe>
+  </>
+ );
 }
 
 export default App;
